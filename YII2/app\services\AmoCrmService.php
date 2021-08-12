a629e683776f137de154f8c1ba65448a52659b45
Spsoi <42588004+Spsoi@users.noreply.github.com>
2021-08-12 18:05:06 +0300

    Create app\services\AmoCrmService.php

---
+++ YII2/app\services\AmoCrmService.php
@@ -0,0 +1,24 @@
+<?php
+// Кастомный файл CURL
+namespace app\services;
+class AmoCrmService
+{
+    public function funCurl ($body) {
+        $link = 'https://Domen.com/amo/tails';
+        // $this->funCurl2($body);
+        $curl = curl_init(); #Сохраняем дескриптор сеанса cURL
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+        curl_setopt($curl, CURLOPT_URL, $link);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($body));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        $out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $code = (int)$code;
+        return $out;
+    }
+}
