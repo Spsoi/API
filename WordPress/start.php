c8c249fca3a934f4a6985d98b25e0dcfa1bcde34
Spsoi <42588004+Spsoi@users.noreply.github.com>
2021-08-13 10:38:19 +0300

    Update start.php

--- WordPress/start.php
+++ WordPress/start.php
@@ -1,1 +1,7 @@
+<?php
+// https://хост.ru/wp-admin
+// Внешний вид -> Редактор Тем -> Функции Темы
+// если работает плагин Contact From 7
+//https://moometric.com/integrations/wp/contact-form-7-zapier-webhook-json-post/
+
 
