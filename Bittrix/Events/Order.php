12d8524f1c7e57ac5c1f681d0df5a54621bdca2c
Spsoi <42588004+Spsoi@users.noreply.github.com>
2021-08-19 11:48:47 +0300

    Create Order.php

---
+++ Bittrix/Events/Order.php
@@ -0,0 +1,40 @@
+<?php
+
+AddEventHandler("sale", "OnOrderAdd", array("AmoAfterUserRegister", "OnAfterIBlockElementAddHandler")); // кастом
+
+class AmoAfterUserRegister
+{
+    function OnAfterIBlockElementAddHandler($arFields)
+    {
+        $data = [
+            'request' => $_REQUEST,
+            'server' => $_SERVER,
+            'files' => $_FILES,
+            'arFields' => $arFields,
+            // 'rs' => $rs
+        ];
+
+        $curl = new Curl;
+        $req = $curl->WebHook($data);
+        return $req;
+    }
+}
+
+class Curl
+{
+    public function WebHook($data) {
+       
+
+        $request = json_encode($data);
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://webhook.site/17ed4399-2199-4e54-916f-b127ccb29088');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($curl, CURLOPT_TIMEOUT_MS, 1100);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS,  ['data' => $request]);
+        $result = curl_exec($curl);
+        curl_close($curl);
+        curl_close ($ch);
+        return $result;
+    }
+}
