76d9271db001646cf4f61d7aec3216021147b2a2
Spsoi <42588004+Spsoi@users.noreply.github.com>
2021-08-19 09:38:51 +0300

    Update User.php

--- Bittrix/Events/User.php
+++ Bittrix/Events/User.php
@@ -1,4 +1,4 @@
 <?php
 // https://dev.1c-bitrix.ru/api_help/main/events/index.php
-// перед тем как выйдет из аккаунта
+// после того как вышел из аккаунта
 AddEventHandler("main", "OnAfterUserLogin", array("AmoAfterUserRegister", "OnAfterIBlockElementAddHandler")); // кастом
