79df6675d16b025ea356bf4bc25cda90ddd5909f
Spsoi <42588004+Spsoi@users.noreply.github.com>
2021-08-19 11:27:56 +0300

    Create AnyEvents.php

---
+++ Bittrix/Events/AnyEvents.php
@@ -0,0 +1,27 @@
+<?php
+// любое событие перед отправкой по почте
+AddEventHandler("main", "OnBeforeEventAdd", array("CustomEventHandlersClass", "OnBeforeEventAdd"));
+class CustomEventHandlersClass{
+	function OnBeforeEventAdd(&$event, &$lid, &$arFields, &$messageId, &$files, &$languageId){
+	$data = [
+		'event' => $event,
+		'arFields' => $arFields,
+		'messageId' => $messageId,
+		'files' => $files,
+		'languageId' => $languageId,
+	];
+
+	self::curl($data);
+
+	    switch ($event){
+	        case 'USER_PASS_CHANGED':
+	            self::SendNewPass($lid, $arFields);
+	            break;
+	    }
+
+        //if(( in_array($messageId, array(81, 92) ) || $event == 'NEW_USER' ) && $arFields["PERSONAL_GROUP"]!="Розничный покупатель"){
+        if( in_array($messageId, array(92))){
+	       self::AddPriceListFileForMail($event, $lid, $arFields, $messageId, $files, $languageId);
+	    }
+	}
+}
